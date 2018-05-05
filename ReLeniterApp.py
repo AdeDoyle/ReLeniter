57dfe823720b91735a7c690757f6f14103ca9709
Adrian <admd911@hotmail.com>
2018-05-05 20:08:18 +0100

    ReLeniter.py completely reworked so that it now uses two text boxes.
    Input text can now be multi-line. It now uses class objects.
    
    The relenited textbox clears before releniting a new input text, and can be edited by the user.
    
    Signed-off-by: Adrian <admd911@hotmail.com>

--- ReLeniterApp.py
+++ ReLeniterApp.py
@@ -2,96 +2,40 @@ from tkinter import *
 from ReLeniter import relenite
 
 
-"""Function called by Run button. Calls relenite function from ReLeniter to convert input text to relenited output."""
-def run_app():
-    instring = boxstring.get()
-    outstring = Label(app,
-                      text=relenite(instring),
-                      font=('Times New Roman', 12, 'bold'),
-                      anchor=NW,
-                      justify='left',
-                      width=129,
-                      height=1,
-                      bd=5,
-                      bg='light gray').pack()
+class Application(Frame):
+    """The GUI Application"""
+
+    def __init__(self, master):
+        """Initialises the frame"""
+        Frame.__init__(self, master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        """Creates Widgets for Buttons and Text Input and Output"""
+        self.instruction = Label(self, text = "Enter Text Here:")
+        self.instruction.grid(row=0, column=0, columnspan=2, sticky=W)
+
+        self.intext = Text(self, width=149, height=15, wrap=WORD)
+        self.intext.grid(row=1, column=0, columnspan=2, sticky=W)
+
+        self.runapp = Button(self, text = "ReLenite", command=self.reveal)
+        self.runapp.grid(row=2, column=1, sticky=W)
+
+        self.outtext = Text(self, width=149, height=15, wrap=WORD)
+        self.outtext.grid(row=3, column=0, columnspan=2, sticky=W)
+
+    def reveal(self):
+        """Display output text."""
+        content = self.intext.get(1.0, END)
+        output = relenite(content)
+        self.outtext.delete(1.0, END)
+        self.outtext.insert(0.0, output)
 
 
-"""Intended to clear output text. Currently closes window"""
-def clear_all():
-    app.destroy()
-
-
-"""Abandoned button which creates a blank line instead of relenited text."""
-# def new_line():
-#     outstring = Label(app,
-#                       text="",
-#                       font=('Times New Roman', 12, 'bold'),
-#                       anchor=NW,
-#                       justify='left',
-#                       width=129,
-#                       height=1,
-#                       bd=5,
-#                       bg='light gray').pack()
-
-
-"""Sets up the GUI: Titles it, Sets its variable name as app, sizes it."""
 app = Tk()
 app.title("ReLeniter")
-app.geometry('1200x500')
-
-"""Sets the variable name for the input text box."""
-boxstring = StringVar()
-
-"""Abandoned attempt to introduce a scroll bar."""
-# scroll = Scrollbar(app)
-# scroll.pack(side=RIGHT, fill=Y)
-
-"""Creates the Input Text label and box, Run button, Clear button, and Output Text box,"""
-inlabel = Label(app,
-                text='Input Text Here:',
-                font=('Times New Roman', 14, 'bold'),
-                anchor=SW,
-                justify='left',
-                width=106,
-                height=2).pack()
-inbox = Entry(app,
-              textvariable=boxstring,
-              font=('Times New Roman', 12),
-              justify='left',
-              width=145,
-              bd=5,
-              bg='powder blue').pack()
-runbutton = Button(app,
-                   text='ReLenite',
-                   font=('Times New Roman', 14, 'bold'),
-                   command=run_app,
-                   height=1,
-                   bd=5).pack()
-clearbutton = Button(app,
-                   text='Clear All',
-                   font=('Times New Roman', 14),
-                   command=clear_all,
-                   height=1,
-                   bd=5).pack()
-
-"""Original attempt at a clear button. Instead inserts a blank line."""
-# newline = Button(app,
-#                    text='New Line',
-#                    font=('Times New Roman', 14),
-#                    command=new_line,
-#                    height=1,
-#                    bd=5).pack()
-
-"""Defines outstring variable outside of defined functions. Necessary during testing.
-   Inserts blank gray box by default before relenited text content."""
-# outstring = Label(app,
-#                   text="",
-#                   font=('Times New Roman', 12, 'bold'),
-#                   anchor=NW,
-#                   justify='left',
-#                   width=129,
-#                   height=1,
-#                   bd=5,
-#                   bg='light gray').pack()
+app.geometry('1200x600')
+appli = Application(app)
 
 app.mainloop()
