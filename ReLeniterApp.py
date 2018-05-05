75050f8e7077d9658cd567d01c9fcd83f0e74e94
Adrian <admd911@hotmail.com>
2018-05-05 16:55:34 +0100

    Update to include ReLeniterApp.py, which will provide a GUI for users. This will be turned into a .exe file at a later stage.
    Update to ReLeniter.py so that it can be used by ReLeniterApp. The input_string variable and print statement at the end which call the function have been commented out.
    
    Explanations added to ReLeniterApp.py by triple quotes.
    
    Signed-off-by: Adrian <admd911@hotmail.com>

--- ReLeniterApp.py
+++ ReLeniterApp.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from ReLeniter import relenite
 
+
+"""Function called by Run button. Calls relenite function from ReLeniter to convert input text to relenited output."""
 def run_app():
     instring = boxstring.get()
     outstring = Label(app,
@@ -13,9 +15,13 @@ def run_app():
                       bd=5,
                       bg='light gray').pack()
 
+
+"""Intended to clear output text. Currently closes window"""
 def clear_all():
     app.destroy()
 
+
+"""Abandoned button which creates a blank line instead of relenited text."""
 # def new_line():
 #     outstring = Label(app,
 #                       text="",
@@ -27,13 +33,20 @@ def clear_all():
 #                       bd=5,
 #                       bg='light gray').pack()
 
+
+"""Sets up the GUI: Titles it, Sets its variable name as app, sizes it."""
 app = Tk()
 app.title("ReLeniter")
-boxstring = StringVar()
 app.geometry('1200x500')
+
+"""Sets the variable name for the input text box."""
+boxstring = StringVar()
+
+"""Abandoned attempt to introduce a scroll bar."""
 # scroll = Scrollbar(app)
 # scroll.pack(side=RIGHT, fill=Y)
 
+"""Creates the Input Text label and box, Run button, Clear button, and Output Text box,"""
 inlabel = Label(app,
                 text='Input Text Here:',
                 font=('Times New Roman', 14, 'bold'),
@@ -60,12 +73,17 @@ clearbutton = Button(app,
                    command=clear_all,
                    height=1,
                    bd=5).pack()
+
+"""Original attempt at a clear button. Instead inserts a blank line."""
 # newline = Button(app,
 #                    text='New Line',
 #                    font=('Times New Roman', 14),
 #                    command=new_line,
 #                    height=1,
 #                    bd=5).pack()
+
+"""Defines outstring variable outside of defined functions. Necessary during testing.
+   Inserts blank gray box by default before relenited text content."""
 # outstring = Label(app,
 #                   text="",
 #                   font=('Times New Roman', 12, 'bold'),
